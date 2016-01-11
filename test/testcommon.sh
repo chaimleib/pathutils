1a2e2a27d866970dfbe81f7146d6a2a6454b3f78
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2016-01-11 00:33:34 -0800

    refactor testcommon.sh, more sh compatibility

--- test/testcommon.sh
+++ test/testcommon.sh
@@ -6,8 +6,18 @@ failed=0
 skipped=0
 total=0
 
-function runsubtests() {
-    dir="$1"
+runtestsatroot() {
+    # run tests in $1/*/*.sh, i.e. tests nested exactly one level deep
+    root="${1?runtestatroot: no root dir provided}"
+
+    for dir in "$root/"*; do
+        [ -d "$dir" ] && runsubtests "$dir"
+    done
+}
+
+runsubtests() {
+    # run tests in this folder
+    dir="${1?runsubtests: no subtest dir provided}"
     echo "Testing `basename $dir` ..."
     for f in "$dir/"*; do
         [ -d "$f" ] && continue
@@ -15,29 +25,23 @@ function runsubtests() {
     done
 }
 
-function runtestcase() {
-    f="$1"
+runtestcase() {
+    # run specified test file
+    f="${1?runtestcase: no test file provided}"
     echo "    $f"
-    ((total++))
+    total=$((total+1))
     if ! [ -x "$f" ]; then
         echo "SKIPPED: $f"
-        ((skipped++))
+        skipped=$((skipped+1))
     elif ! . "$f"; then
         echo "FAILED:  $f"
-        ((failed++))
+        failed=$((failed+1))
     fi
 }
 
-function runtestsatroot() {
-    root="${1?runtestatroot: no root dir provided}"
-
-    for dir in "$root/"*; do
-        [[ -d "$dir" ]] && runsubtests "$dir"
-    done
-}
-
-function finishtests() {
-    if [[ "$failed" == 0 ]]; then
+finishtests() {
+    # print report summary and give exit code
+    if [ "$failed" -eq 0 ]; then
         echo "No failures in $total tests, $skipped skipped. Bravo!"
         echo "SUCCESS"
     else
